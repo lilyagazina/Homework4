d6f22694f15b703e5367a8d8c74af8de087452f0
Лилиана <lilya_gazina@mail.ru>
2023-01-26 19:45:19 +0300

    изменена

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
-        task2();
         task3();
         task4();
         task5();
         task6();
         task7();
     }
-    public static void task1() {
-        System.out.println("Задача 1");
-        int age = 24;
-        if (age >= 18) {
-            System.out.println("Если возраст человека равен " +age+ ", то он совершеннолетний");
-        } else {
-            System.out.println("Если возраст человека равен " +age+ ", он не достиг совершеннолетия, нужно немного подождать");
-        }
 
-    }
-    public static void task2() {
-        System.out.println("Задача 2");
-        int temp = 7;
-        if (temp < 5) {
-            System.out.println("На улице " +temp+ " градусов, нужно надеть шапку");
-        } else {
-            System.out.println("На улице " +temp+ " градусов, можно идти без шапки»");
-        }
-    }
     public static void task3() {
         System.out.println("Задача 3");
         int speed = 90;
